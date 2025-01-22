c39c17a5431205a80f26487c7e815c667c7533e9
ranjit <ranjitkumarnayak420@gmail.com>
2025-01-22 08:16:28 +0530

    clerk initial setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from '@/components/ui/button'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <Button>login</Button>
+    <div className='m-10'>
+      <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
     </div>
   )
 }
